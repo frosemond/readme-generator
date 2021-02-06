9c83ae42e746a1bcd54a076045eb00539076e39d
fazle <ryan7998@gmail.com>
2021-02-05 22:16:15 -0500

    add link to license

--- Develop/dist/README.md
+++ Develop/dist/README.md
@@ -1,6 +1,6 @@
 # asdf
 
-  ![alt text](https://img.shields.io/static/v1?label=licence&message=GNU%20AGPLv3%2C%20Mozilla%20Public%20License%202.0%2C%20Apache%20License%202.0&color=GREEN)
+  ![alt text](https://img.shields.io/static/v1?label=licence&message=GNU%20GPLv3&color=GREEN)
 
   ## Description
   $//{data.description}
@@ -30,12 +30,10 @@
 
 
   ## License
-  GNU AGPLv3: <br />GNU AGPLv3: Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available 
-<br />Mozilla Public License 2.0: <br />Mozilla Public License 2.0: Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.
-<br />Apache License 2.0: <br />Apache License 2.0: A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code. 
+  GNU GPLv3: <br />[](../src/GNU GPLv3)Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. 
 <br />
 
 
-  ## Questioins
+  ## Questions
   Github Profile: $//{data.githubUsername}
   Reach me with additional questions at $//{data.email}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,8 +13,8 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-
+function renderLicenseLink(element) {
+  return '[](../src/' + element + ')';
 }
 
 // TODO: Create a function that returns the license section of README
@@ -24,30 +24,31 @@ function renderLicenseSection(license) {
   license.forEach(element => {
     console.log(element);
     licenceText += element + ': <br />';
+    licenceText += renderLicenseLink(element);
     switch(element){
       case 'GNU AGPLv3':
-        licenceText += 'GNU AGPLv3: Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available \n';
+        licenceText += 'Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available \n';
         break;
       case 'GNU GPLv3':
-        licenceText += 'GNU GPLv3: Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. \n';
+        licenceText += 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. \n';
         break;
       case 'GNU LGPLv3':
-        licenceText += 'GNU LGPLv3: Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work \n';
+        licenceText += 'Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work \n';
         break;
       case 'Mozilla Public License 2.0':
-        licenceText += 'Mozilla Public License 2.0: Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.\n';
+        licenceText += 'Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.\n';
         break;
       case 'Apache License 2.0':
-        licenceText += 'Apache License 2.0: A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code. \n';
+        licenceText += 'A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code. \n';
         break;
       case 'MIT License':
-        licenceText += 'MIT License: A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code. \n';
+        licenceText += 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code. \n';
         break;
       case 'Boost Software License 1.0':
-        licenceText += 'Boost Software License 1.0: \n A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code. \n';
+        licenceText += 'A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code. \n';
         break;
       case 'The Unlicense':
-        licenceText += 'The Unlicense: A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code. \n';
+        licenceText += 'A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code. \n';
         break;
       default:
         licenceText +='';
@@ -102,7 +103,7 @@ function generateMarkdown(data) {
   ${renderLicenseSection(data.license)}
 
 
-  ## Questioins
+  ## Questions
   Github Profile: $//{data.githubUsername}
   Reach me with additional questions at $//{data.email}
 `;
